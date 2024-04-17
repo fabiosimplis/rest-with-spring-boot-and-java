16bf448446c8dd8d30c7c023c4baf8c506d4bd49
fabiosimplis <fabio.simplis@gmail.com>
2024-04-17 20:56:25 -0300

    ADD Migrations with FlyWay

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+    (1, 'Oscar', 'Schmidt', 'São Paulo', 'Male'),
+    (2, 'Tom', 'Brady', 'Boston', 'Male'),
+    (3, 'Jhon', 'Wick', 'Nova York', 'Male');
