48d1c5cff327d889f81c6bad865d6ea40e47b741
fabiosimplis <fabio.simplis@gmail.com>
2024-04-17 21:27:57 -0300

    ADD Content Negotiation via Query Parameter

---
+++ src/main/java/br/com/erudio/config/WebConfig.java
@@ -0,0 +1,24 @@
+package br.com.erudio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // Via Query Param. http://localhost:8080/api/person/v1?mediaType=xml
+
+        configurer.favorParameter(true)//Aceita parametros
+                .parameterName("mediaType")//nome do parametro
+                .ignoreAcceptHeader(true)//
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
