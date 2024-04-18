c106d295c5bbc10fcf5cfc887cc30104a1ebab37
fabiosimplis <fabio.simplis@gmail.com>
2024-04-17 21:33:11 -0300

    ADD Content Negotiation via HEADER Parameter

--- src/main/java/br/com/erudio/config/WebConfig.java
+++ src/main/java/br/com/erudio/config/WebConfig.java
@@ -13,9 +13,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         // Via Query Param. http://localhost:8080/api/person/v1?mediaType=xml
 
-        configurer.favorParameter(true)//Aceita parametros
+        /*configurer.favorParameter(true)//Aceita parametros
                 .parameterName("mediaType")//nome do parametro
                 .ignoreAcceptHeader(true)//
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+
+        // Via HEADER PARAM. http://localhost:8080/api/person/v1
+        //Content Negotiation via Header Parameter
+        configurer.favorParameter(false)//Aceita parametros
+                .ignoreAcceptHeader(false)//
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
