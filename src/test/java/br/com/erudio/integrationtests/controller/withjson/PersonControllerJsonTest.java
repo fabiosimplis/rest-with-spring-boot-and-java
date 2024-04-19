71ead1e4cc63f22fa18a44a3795812852cbd59f5
fabiosimplis <fabio.simplis@gmail.com>
2024-04-19 15:56:49 -0300

    ADD validation CORS integration tests

---
+++ src/test/java/br/com/erudio/integrationtests/controller/withjson/PersonControllerJsonTest.java
@@ -0,0 +1,88 @@
+package br.com.erudio.integrationtests.controller.withjson;
+
+import br.com.erudio.configs.TestConfigs;
+import br.com.erudio.integationtests.vo.PersonVO;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PersonControllerJsonTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+
+    private static PersonVO person;
+
+    @BeforeAll
+    public static void setup(){
+        objectMapper = new ObjectMapper();
+        //Configura comportamento ao receber o JSON para ignorar campos HATEOAS
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        person = new PersonVO();
+    }
+
+    @Test
+    @Order(1)
+    void testCreate() throws JsonProcessingException {
+        mockPerson();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, "https://erudio.com.br")
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))//filtro para logar as requisições
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))//filtro para logar as respostas
+                .build();
+
+        var content =
+                given().spec(specification)
+                        .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                        .body(person)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .asString();
+
+        PersonVO createdPerson = objectMapper.readValue(content, PersonVO.class);
+        person = createdPerson;
+
+        assertNotNull(createdPerson);
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+
+        assertTrue(createdPerson.getId() > 0);
+
+        assertEquals("Richard", createdPerson.getFirstName());
+        assertEquals("Stallman", createdPerson.getLastName());
+        assertEquals("New York City, New York, US", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+    }
+
+    private void mockPerson() {
+        person.setFirstName("Richard");
+        person.setLastName("Stallman");
+        person.setAddress("New York City, New York, US");
+        person.setGender("Male");
+    }
+
+}
