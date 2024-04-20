9e92a206c2b5c4537382ce7da148c020b1527cb3
fabiosimplis <fabio.simplis@gmail.com>
2024-04-20 07:55:38 -0300

    ADD Spring Security and news Entities

---
+++ src/main/java/br/com/erudio/model/User.java
@@ -0,0 +1,87 @@
+package br.com.erudio.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails, Serializable {
+
+    private static final long serialVersioUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_name", unique = true)
+    private String userName;
+    @Column(name = "Full_name")
+    private String fullName;//Não é necessário para os dados do user, mas foi colocado
+    @Column(name = "password")
+    private String password;
+    @Column(name = "account_non_expired")
+    private boolean accountNonExpired;
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked;
+    @Column(name = "credentials_non_expired")
+    private boolean credentialsNonLocked;
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission",
+        joinColumns = {@JoinColumn (name = "id_user")},
+        inverseJoinColumns = {@JoinColumn (name = "id_permission")}
+    )
+    private List<Permission> permissions;
+
+    public User() {}
+
+    public List<String> getRoles(){
+        List<String> roles = new ArrayList<>();
+        for (Permission permission : permissions){
+            roles.add(permission.getDescription());
+        }
+        return roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.permissions;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
