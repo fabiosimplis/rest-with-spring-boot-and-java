018c082051960d9f33d0edc3b510a69b943f7110
fabiosimplis <fabio.simplis@gmail.com>
2024-04-21 19:42:56 -0300

    ADD Controller Book integrations tests with JSON

---
+++ src/test/java/br/com/erudio/integrationtests/controller/withjson/BookControllerJsonTest.java
@@ -0,0 +1,269 @@
+package br.com.erudio.integrationtests.controller.withjson;
+
+import br.com.erudio.configs.TestConfigs;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import br.com.erudio.integrationtests.vo.AccountCredentialsVO;
+import br.com.erudio.integrationtests.vo.BookVO;
+import br.com.erudio.integrationtests.vo.TokenVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.List;
+
+import static br.com.erudio.configs.TestConfigs.ORIGIN_ERUDIO;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BookControllerJsonTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+
+    private static BookVO book;
+
+    @BeforeAll
+    public static void setup(){
+        objectMapper = new ObjectMapper();
+        //Configura comportamento ao receber o JSON para ignorar campos HATEOAS
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        book = new BookVO();
+    }
+
+    @Test
+    @Order(0)
+    public void authorization() throws JsonMappingException, JsonProcessingException {
+        AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin123");
+
+        var accessToken = given()
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(user)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenVO.class).getAccessToken();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + accessToken)
+                .setBasePath("/api/book/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))//filtro para logar as requisições
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))//filtro para logar as respostas
+                .build();
+    }
+
+
+    @Test
+    @Order(1)
+    public void testCreate() throws JsonMappingException, JsonProcessingException {
+        mockBook();
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(book)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        // Como o restassured usa uma abstração sobre objectmapper do Jackson ocorre um erro
+        // Convertemos para string para melhor realização dos testes
+
+        BookVO persisteBook = objectMapper.readValue(content, BookVO.class);
+        book = persisteBook;
+
+        assertNotNull(persisteBook);
+        assertNotNull(persisteBook.getId());
+        assertNotNull(persisteBook.getAuthor());
+        assertNotNull(persisteBook.getLaunchDate());
+        assertNotNull(persisteBook.getTitle());
+        assertNotNull(persisteBook.getPrice());
+
+        assertTrue(persisteBook.getId() > 0);
+
+        assertEquals("Joshua Bloch", persisteBook.getAuthor());
+        assertEquals("Java Efetivo", persisteBook.getTitle());
+        assertEquals(80D, persisteBook.getPrice());
+
+    }
+
+    @Test
+    @Order(2)
+    public void testUpdate() throws JsonMappingException, JsonProcessingException {
+        book.setPrice(86D);
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(book)
+                .when()
+                .put()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        // Como o restassured usa uma abstração sobre objectmapper do Jackson ocorre um erro
+        // Convertemos para string para melhor realização dos testes
+
+        BookVO persisteBook = objectMapper.readValue(content, BookVO.class);
+        book = persisteBook;
+
+        assertNotNull(persisteBook);
+        assertNotNull(persisteBook.getId());
+        assertNotNull(persisteBook.getAuthor());
+        assertNotNull(persisteBook.getLaunchDate());
+        assertNotNull(persisteBook.getTitle());
+        assertNotNull(persisteBook.getPrice());
+
+        assertEquals(book.getId(), persisteBook.getId());
+
+        assertEquals("Joshua Bloch", persisteBook.getAuthor());
+        assertEquals("Java Efetivo", persisteBook.getTitle());
+        assertEquals(86D, persisteBook.getPrice());
+    }
+
+    @Test
+    @Order(3)
+    public void testFindById() throws JsonMappingException, JsonProcessingException {
+        mockBook();
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, ORIGIN_ERUDIO)
+                .pathParams("id", book.getId())
+                .when()
+                .get("{id}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+        // Como o restassured usa uma abstração sobre objectmapper do Jackson ocorre um erro
+        // Convertemos para string para melhor realização dos testes
+
+        BookVO persisteBook = objectMapper.readValue(content, BookVO.class);
+        book = persisteBook;
+
+        assertNotNull(persisteBook);
+        assertNotNull(persisteBook.getId());
+        assertNotNull(persisteBook.getAuthor());
+        assertNotNull(persisteBook.getLaunchDate());
+        assertNotNull(persisteBook.getTitle());
+        assertNotNull(persisteBook.getPrice());
+
+        assertTrue(persisteBook.getId() > 0);
+
+        assertEquals("Joshua Bloch", persisteBook.getAuthor());
+        assertEquals("Java Efetivo", persisteBook.getTitle());
+        assertEquals(86D, persisteBook.getPrice());
+    }
+
+    @Test
+    @Order(4)
+    public void testDelete() throws JsonMappingException, JsonProcessingException {
+
+        given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .pathParams("id", book.getId())
+                .when()
+                .delete("{id}")
+                .then()
+                .statusCode(204);
+
+    }
+
+    @Test
+    @Order(5)
+    public void testFindAll() throws JsonMappingException, JsonProcessingException {
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().asString();
+                /*.as(new TypeRef<List<PersonVO>>() {
+                });*/
+        // Como o restassured usa uma abstração sobre objectmapper do Jackson ocorre um erro
+        // Convertemos para string para melhor realização dos testes
+        List<BookVO> people = objectMapper.readValue(content, new TypeReference<List<BookVO>>() {});
+        BookVO foundBookOne = people.get(0);
+
+        assertNotNull(foundBookOne.getId());
+        assertNotNull(foundBookOne.getLaunchDate());
+        assertNotNull(foundBookOne.getPrice());
+        assertNotNull(foundBookOne.getAuthor());
+        assertNotNull(foundBookOne.getTitle());
+
+        assertEquals(1, foundBookOne.getId());
+
+        assertEquals(49D, foundBookOne.getPrice());
+        assertEquals("Michael C. Feathers", foundBookOne.getAuthor());
+        assertEquals("Working effectively with legacy code", foundBookOne.getTitle());
+
+        BookVO foundBookTwo = people.get(3);
+
+        assertNotNull(foundBookTwo.getId());
+        assertNotNull(foundBookTwo.getTitle());
+        assertNotNull(foundBookTwo.getAuthor());
+        assertNotNull(foundBookTwo.getPrice());
+
+
+        assertEquals(4, foundBookTwo.getId());
+
+        assertEquals("Crockford", foundBookTwo.getAuthor());
+        assertEquals(67D, foundBookTwo.getPrice());
+        assertEquals("JavaScript", foundBookTwo.getTitle());
+
+    }
+
+    @Test
+    @Order(6)
+    public void testFindAllWithoutToken() throws JsonMappingException, JsonProcessingException {
+
+        RequestSpecification specificationWithoutToken = new RequestSpecBuilder()
+                .setBasePath("/api/book/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))//filtro para logar as requisições
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))//filtro para logar as respostas
+                .build();
+
+         given().spec(specificationWithoutToken)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(403);
+                
+    }
+
+    private void mockBook() {
+        book.setAuthor("Joshua Bloch");
+        book.setLaunchDate(new Date());
+        book.setTitle("Java Efetivo");
+        book.setPrice(80D);
+    }
+
+}
---
+++ src/test/java/br/com/erudio/integrationtests/vo/BookVO.java
@@ -0,0 +1,87 @@
+package br.com.erudio.integrationtests.vo;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+
+public class BookVO extends RepresentationModel<BookVO> implements Serializable {
+
+    private static final long serialVersioUID = 1L;
+
+    private Long id;
+
+    private String author;
+
+    private Date launchDate;
+
+    private Double price;
+
+    private String title;
+
+    public BookVO(){}
+
+    public BookVO(Long id, String author, Date launchDate, Double price, String title) {
+        this.id = id;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        BookVO bookVO = (BookVO) o;
+        return Objects.equals(id, bookVO.id) && Objects.equals(author, bookVO.author) && Objects.equals(launchDate, bookVO.launchDate) && Objects.equals(price, bookVO.price) && Objects.equals(title, bookVO.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, author, launchDate, price, title);
+    }
+}
