db232354d86af8c6a81ba483c947347d8968479b
fabiosimplis <fabio.simplis@gmail.com>
2024-04-22 11:31:03 -0300

    UPDATE Migration

--- src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,4 +1,10 @@
 INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
     (1, 'Oscar', 'Schmidt', 'São Paulo', 'Male'),
     (2, 'Tom', 'Brady', 'Boston', 'Male'),
-    (3, 'Jhon', 'Wick', 'Nova York', 'Male');
+    (3, 'Jhon', 'Wick', 'Nova York', 'Male'),
+    (4, 'Leonardo', 'da Vinci','Anchiano - Italy', 'Male'),
+    (5, 'Indira', 'Gandhi', 'Porbandar - India', 'Female'),
+    (6, 'Mahatma', 'Gandhi', 'Porbandar - India', 'Male'),
+    (7, 'Muhammad', 'Ali', 'Kentucky - US', 'Male'),
+    (8, 'Nelson', 'Mvezo', 'Mvezo – South Africa', 'Male'),
+    (9, 'Nikola', 'Tesla', 'Smiljan - Croácia', 'Male');
